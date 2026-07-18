841ec66f6b41cd21e2ab91a740a93192cacbe9cd
Mohit-codex <kumarmohit9343@gmail.com>
2026-07-18 11:23:40 +0530

    date-18-07-2026

---
+++ starting java script/index.js
@@ -0,0 +1,32 @@
+// console.log("this is my first js code");
+// console.log("this is my second line");
+var a=10;
+console.log(a);
+function example(){
+    var b=100;
+    console.log(a);
+    console.log(b);
+}
+example();
+// console.log(b);
+var a="mohit";
+ console.log(a);
+ a="syaam"
+  console.log(a);
+  let num=20;
+   num=40;
+   console.log(num);
+{
+    let d=10;
+     console.log(num);
+}
+//  console.log(d);
+const c=200;
+ console.log(c);
+//  const c=12;
+//  c=29;
+console.log(typeof(c));
+console.log(typeof(a));
+if(true){
+     console.log(c);
+}
